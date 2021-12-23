d17a0061c0a64a587fd5dd1703d2e515082df01e
Daniel <krivelevdaniil@gmail.com>
2021-12-23 12:09:00 +0700

    Initial Commit

---
+++ SwiftUIPhotoMetadata/SwiftUIPhotoMetadataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoMetadataApp.swift
+//  SwiftUIPhotoMetadata
+//
+//  Created by Developer on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoMetadataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
